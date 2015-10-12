b698c8ff8666ea2762050c079fb4d45df946598d
chengz <stridercheng@163.com>
2015-10-12 22:05:56 +0800

    add movie activity

---
+++ app/src/main/java/com/android/noflag/play/activity/MovieActivity.java
@@ -0,0 +1,23 @@
+package com.android.noflag.play.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.noflag.play.R;
+
+/**
+ * description:
+ * User: stridercheng
+ * Date: 2015-10-12
+ * Time: 21:59
+ * FIXME
+ */
+public class MovieActivity extends AppCompatActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+    }
+}
--- app/src/main/java/com/android/noflag/play/fragment/MovieFragment.java
+++ app/src/main/java/com/android/noflag/play/fragment/MovieFragment.java
@@ -9,9 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.noflag.play.MyApplication;
